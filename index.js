82f94171c2c410a9cfefb60c1315ae4584be21f8
Liran Tal <liran.tal@nielsen.com>
2017-02-13 00:17:04 +0200

    docs(index.js): adding docblock comments to main program

--- index.js
+++ index.js
@@ -4,10 +4,20 @@ const glob = require('glob')
 const fs = require('fs')
 const Promise = require('bluebird')
 
+/**
+ * Constructor function for the LicensesCheck module
+ *
+ * @param {string} filesPattern glob syntax for matching package.json files, for example: node_modules/ ** /package.json
+ * @constructor
+ */
 function LicenseCheck(filesPattern = 'node_modules/**/package.json') {
   this.filesPattern = filesPattern
 }
 
+/**
+ * Returns a list of all matched files based on glob pattern passed to the LicenseCheck constructor
+ * @returns {Promise} globPromise resolved with an array of file paths
+ */
 LicenseCheck.prototype.listFiles = function () {
 
   if (typeof this.filesPattern !== 'string') {
@@ -22,6 +32,13 @@ LicenseCheck.prototype.listFiles = function () {
   return globPromise(this.filesPattern)
 }
 
+/**
+ * A reducer which essentially creates a hashmap object of licenses aggregated
+ * based on the count of their appearance in a licenses array
+ *
+ * @param {array} licenses string array of license names
+ * @returns {Object} normalizedLicenses
+ */
 LicenseCheck.prototype.normalize = function (licenses = []) {
   if (Array.isArray(licenses) !== true) {
     return false
@@ -43,9 +60,15 @@ LicenseCheck.prototype.normalize = function (licenses = []) {
   return normalizedLicenses
 }
 
+/**
+ * Asynchronously read contents of all package.json files from array of files
+ * and parse the license information from files
+ *
+ * @param {array} files
+ * @return {Promise} licensePromises resolved to an array of licenses
+ */
 LicenseCheck.prototype.licenses = function (files = []) {
 
-  let licenseHash = {}
   let licensePromises = []
 
   files.forEach((item) => {
